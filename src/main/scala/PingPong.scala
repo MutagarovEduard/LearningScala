cc8a3ab63370729419499e17e12d440f7e296dea
rinat.isangulov <rinat.isangulov@gost-group.com>
2019-12-16 16:46:12 +0500

    PingPong

--- src/main/scala/PingPong.scala
+++ src/main/scala/PingPong.scala
@@ -1,31 +1,49 @@
+
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.util.Timeout
+
+import scala.concurrent.duration._
+import scala.util.{Failure, Success}
+
 object PingPong extends App{
   sealed trait PingPongCommand
-  case class Start(pongRef:ActorRef) extends PingPongCommand
+  case class Start() extends PingPongCommand
   case class Stop() extends PingPongCommand
   case class Ping() extends PingPongCommand
   case class Pong() extends PingPongCommand
 
+
+  implicit val postfix = scala.language.postfixOps
+  implicit val system = ActorSystem("Sys")
+  implicit val timeout = Timeout(1 second)
+  implicit val exContext = system.dispatcher
+
+
   class PingActor(maxCount:Int) extends Actor {
-    var pongRef:ActorRef = ActorRef.noSender;
+    var pongRef1:ActorRef = ActorRef.noSender;
     var count:Int = 0
     def countPlus: Unit = {
       count+=1
     }
 
     def receive:Actor.Receive = {
-      case Start(pongRef) =>
-        this.pongRef = pongRef
-        countPlus
-        print("ping ")
-        pongRef ! Pong
+      case Start =>
+        sys.actorSelection("user/pong/").resolveOne().onComplete {
+          case Success(actorRef) =>
+            this.pongRef1 = actorRef
+            countPlus
+            print("ping ")
+            pongRef1 ! Pong
+          case Failure(exception) => println(exception)
+        }
+
       case Ping =>
         if (count<maxCount) {
           countPlus
           print("ping ")
-          pongRef ! Pong
+          pongRef1 ! Pong
         } else {
-          pongRef ! Stop
+          pongRef1 ! Stop
           println("Ping stopped")
           context.stop(self)
         }
@@ -42,9 +60,9 @@ object PingPong extends App{
     }
   }
   val sys = ActorSystem("system")
-  val pong = sys.actorOf(Props(new PongActor))
-  val ping = sys.actorOf(Props(new PingActor(10)))
-  ping ! Start(pong)
+  val pong = sys.actorOf(Props(new PongActor),"pong")
+  val ping = sys.actorOf(Props(new PingActor(10)),"ping")
+  ping ! Start
   sys.terminate()
 }
 
