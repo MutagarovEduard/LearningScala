ddd55e455cc97a1b610e83885f06dc0d46fbb3ee
rinat.isangulov <rinat.isangulov@gost-group.com>
2019-12-16 15:57:34 +0500

    PingPong

--- src/main/scala/PingPong.scala
+++ src/main/scala/PingPong.scala
@@ -1,18 +1,21 @@
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 object PingPong extends App{
   sealed trait PingPongCommand
-  case class Start() extends PingPongCommand
+  case class Start(pongRef:ActorRef) extends PingPongCommand
   case class Stop() extends PingPongCommand
   case class Ping() extends PingPongCommand
   case class Pong() extends PingPongCommand
 
-  class PingActor(maxCount:Int, pongRef:ActorRef) extends Actor {
+  class PingActor(maxCount:Int) extends Actor {
+    var pongRef:ActorRef = ActorRef.noSender;
     var count:Int = 0
     def countPlus: Unit = {
       count+=1
     }
+
     def receive:Actor.Receive = {
-      case Start =>
+      case Start(pongRef) =>
+        this.pongRef = pongRef
         countPlus
         print("ping ")
         pongRef ! Pong
@@ -40,8 +43,8 @@ object PingPong extends App{
   }
   val sys = ActorSystem("system")
   val pong = sys.actorOf(Props(new PongActor))
-  val ping = sys.actorOf(Props(new PingActor(10,pong)))
-  ping ! Start
+  val ping = sys.actorOf(Props(new PingActor(10)))
+  ping ! Start(pong)
   sys.terminate()
 }
 
